56b884915c18bfea983a24d08d3619ada87515a4
kuku <jung.jessica@outlook.com>
2019-08-06 00:27:07 +0800

    feat: home page UI finished

--- homepage/vue.config.js
+++ homepage/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     devServer: {
         disableHostCheck: true,
-    }
+    },
+    chainWebpack: config => {
+        config.module
+            .rule("css")
+            .use("px2rem-loader")
+                .loader("px2rem-loader")
+                .tap(options => {
+                options = {remUnit : 75};
+                return options;
+            });
+    },
 };
